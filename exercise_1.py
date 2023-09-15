49374767cf6aad08421c2527e0aa3e76d96c161c
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2023-09-15 02:01:34 -0400

    update

--- exercise_1.py
+++ exercise_1.py
@@ -1,5 +1,20 @@
 fib_cache = {}
 
+
+# def memoize(f):
+#     cache = {}
+#     count = 0 
+#     def kendrick_lamar(num):
+#         if num not in cache:
+#             cache[num] = f(num)
+#         return cache[num], count
+#     return kendrick_lamar
+
+
+
+
+
+# @memoize
 def fib_memo(num):
     if num <= 1:
         return 1
@@ -7,7 +22,14 @@ def fib_memo(num):
         fib_cache[num] = fib_memo(num - 1) + fib_memo(num - 2)
     return fib_cache[num]
 
+
+print(fib_memo(19))
 print(fib_memo(9))
+print(fib_memo(6))
+print(fib_memo(39))
+print(fib_memo(41))
+print(fib_memo(59))
+
 
 
 
